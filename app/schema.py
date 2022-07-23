c0d3a9886fb76b0b2d0cc4bf81887875a9a80f99
ra1993 <richardamin93@gmail.com>
2022-07-23 13:25:09 -0400

    fixing schema import error

--- app/schema.py
+++ app/schema.py
@@ -4,10 +4,6 @@ from app.schemas.poke_schema import Generation
 from app.schemas.pie_chart_schema import GenPieChart, Num_Pokemon
 from app.transform_data import generation_groupby
 
-@strawberry.type
-class Query:
-    PokeDex: Generation = strawberry.field(resolver=process_pokedex)
-
 def get_pie_chart_data()-> GenPieChart:
     pie_chart_data=[]
     gen_pie=[]
@@ -30,12 +26,16 @@ def get_pie_chart_data()-> GenPieChart:
         chart_data=pie_chart_data
         )
 )
-# print(get_pie_chart_data())
-# print(type(get_pie_chart_data()))
+
 
 @strawberry.type
-class Query2:
+class Query:
+    PokeDex: Generation = strawberry.field(resolver=process_pokedex)
     GenBreakDown : GenPieChart = strawberry.field(resolver=get_pie_chart_data) 
 
 
-schema = strawberry.Schema(query=Query2)
+# print(get_pie_chart_data())
+# print(type(get_pie_chart_data()))
+
+
+schema = strawberry.Schema(query=Query)
